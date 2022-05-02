e9b77961dc0689ab86e152a01318701c97e0ea86
Manabu OHTAKE <manabu2783@hotmail.com>
2022-05-03 08:22:01 +0900

    feat: :tada: first commit

---
+++ components/milkdown/MilkdownEditor.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect, RefObject } from 'react'
+
+import { defaultValueCtx, Editor, rootCtx } from '@milkdown/core'
+import { nord } from '@milkdown/theme-nord'
+import { ReactEditor, useEditor } from '@milkdown/react'
+import { commonmark } from '@milkdown/preset-commonmark'
+import { emoji } from '@milkdown/plugin-emoji'
+
+export const MilkdownEditor = ({
+  content,
+}: { content: string }) => {
+  const editor = useEditor(
+    (root, renderReact) => {
+      return Editor.make()
+        .config((ctx) => {
+          ctx.set(rootCtx, root)
+          ctx.set(defaultValueCtx, content)
+        })
+        .use(nord)
+        .use(emoji)
+        .use(commonmark)
+    },
+  )
+  return (
+    <ReactEditor editor={editor} />
+  )
+}
